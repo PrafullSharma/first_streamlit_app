0c34f1142bed426be6a46fe7931a7c366744adc9
Prafull Sharma <prafull@ymail.com>
2023-08-25 18:25:04 +0530

    Update streamlit_app.py
    show normalized fruityvice_response in data frame

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,3 +25,7 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
 streamlit.text(fruityvice_response.json())
+# normalize fruityvice_response
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# show normalized fruityvice_response in data frame
+streamlit.dataframe(fruityvice_normalized)
