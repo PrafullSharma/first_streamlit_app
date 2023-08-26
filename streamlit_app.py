4ebbe98ea62401d8cf61b72aea505e9a64d0a996
Prafull Sharma <prafull@ymail.com>
2023-08-26 20:00:09 +0530

    Update streamlit_app.py
    code optimization with new function

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,18 +23,21 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table of selected fruits.
 streamlit.dataframe(fruits_to_show)
 
+
+# define function get_fruityvice_data - parameter this_fruit_coice
+    def get_fruityvice_data(this_fruit_coice):
+      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+      return fruityvice_normalized
 # Fruityvice Fruit Advice!
 streamlit.header("Fruityvice Fruit Advice!")
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
-  streamlit.write('The user entered ', fruit_choice)
   if not fruit_choice:
     streamlit.error("Please select a fruit to get information.")
   else:
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    streamlit.dataframe(fruityvice_normalized)
-
+    back_from_function = get_fruityvice_data(fruit_choice)
+    streamlit.dataframe(back_from_function)
 except URLError as e:
   streamlit.error()
 
